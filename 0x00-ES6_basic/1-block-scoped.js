c33cdc369104273f3b3be616893e6cddaba0d174
syphrose <swabulwenyi@gmail.com>
2024-01-03 17:47:25 +0000

    Conditional block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
+/* modifying variables*/
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+	  /*disable eslint*/
    const task = true;
    const task2 = false;
   }
