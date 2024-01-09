5871c8949975cf5178cafea88bc2720ad2548081
syphrose <swabulwenyi@gmail.com>
2024-01-09 19:36:33 +0300

    the .js test files

---
+++ 0x02-ES6_classes/0-main.js
@@ -0,0 +1,7 @@
+import ClassRoom from "./0-classroom.js";
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize)import ClassRoom from "./0-classroom.js";
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize)
